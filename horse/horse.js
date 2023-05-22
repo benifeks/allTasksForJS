a542fed6fe4787dae2f5664a36317c32e6446548
Sergey <benifeks72@gmail.com>
2023-05-22 12:08:08 +0300

    added totalMileage

--- horse/horse.js
+++ horse/horse.js
@@ -1,11 +1,15 @@
+
 function Horse(name = 'Anonim') {
 
   this.name = name;
   let _mileage = 0;
+  Horse.prototype.totalMileage = 0;
 
   this.run = function(kilometers = 0) {
+
     _mileage = _mileage + kilometers;
-    return _mileage;
+    Horse.prototype.totalMileage = Horse.prototype.totalMileage + kilometers;
+
   };
 
   this.getMileage = function() {
@@ -18,33 +22,12 @@ let horseAnonim = new Horse();
 let horseBlack = new Horse('Black');
 let horseWhite = new Horse('White');
 
-
-console.log(`Пробег ${horseAnonim.name} - ${horseAnonim.getMileage()}`)
-console.log(`Пробег ${horseBlack.name} - ${horseBlack.getMileage()}`)
-console.log(`Пробег ${horseWhite.name} - ${horseWhite.getMileage()}`)
-
 horseAnonim.run(7);
 horseAnonim.run(3);
 horseBlack.run(3);
 horseBlack.run(7);
 horseWhite.run(5);
-console.log(`---------`)
 
-console.log(`Пробег ${horseAnonim.name} - ${horseAnonim.getMileage()}`)
-console.log(`Пробег ${horseBlack.name} - ${horseBlack.getMileage()}`)
-console.log(`Пробег ${horseWhite.name} - ${horseWhite.getMileage()}`)
-
-let horseGreen = new Horse('Green');
-
-horseGreen.run(5);
-horseAnonim.run(10);
-horseBlack.run(10);
-horseWhite.run(10);
-
-console.log(`---------`)
-console.log(`Пробег ${horseAnonim.name} - ${horseAnonim.getMileage()}`)
-console.log(`Пробег ${horseBlack.name} - ${horseBlack.getMileage()}`)
-console.log(`Пробег ${horseWhite.name} - ${horseWhite.getMileage()}`)
-console.log(`Пробег ${horseGreen.name} - ${horseGreen.getMileage()}`)
-
-console.log(`Пробег ${horseGreen.name} - ${horseGreen._mileage}`)
+console.log(`Общий пробег у ${horseAnonim.name} - ${horseAnonim.totalMileage}`)
+console.log(`Общий пробег у  ${horseBlack.name} - ${horseBlack.totalMileage}`)
+console.log(`Общий пробег у  ${horseWhite.name} - ${horseWhite.totalMileage}`)
